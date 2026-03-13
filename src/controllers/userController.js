17694ee3d97dcbef53d330f615ebd6be94c984c6
auxiliarbd <auxiliar.bd@globalstoreonline.com>
2026-03-13 11:24:24 -0600

    fix: improvement in the response structure

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,38 +1,141 @@
+const bcrypt = require("bcrypt");
 const userModel = require("../models/userModel");
 
-exports.createUser = async (req, res) => {
-  const { name, email } = req.body;
-
-  const user = await userModel.createUser(name, email);
-
-  res.status(201).json(user);
-};
-
 exports.getUsers = async (req, res) => {
-  const users = await userModel.getUsers();
-  res.json(users);
+  try {
+    const users = await userModel.getUsers();
+
+    if (users.length === 0) {
+      res.status(404).json({
+        success: false,
+        messaje: "empty table",
+      });
+    }
+
+    const formattedUsers = users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    }));
+
+    res.status(200).json({
+      success: true,
+      users: formattedUsers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 exports.getUser = async (req, res) => {
-  const user = await userModel.getUserById(req.params.id);
+  try {
+    const user = await userModel.getUserById(req.params.id);
 
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    }
+
+    const formattedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    res.status(200).json({
+      success: true,
+      user: formattedUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
+};
 
-  res.json(user);
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.createUser(name, email, hashedPassword);
+    const formattedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    res.status(201).json({
+      success: true,
+      user: formattedUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 exports.updateUser = async (req, res) => {
-  const { name, email } = req.body;
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.updateUser(
+      req.params.id,
+      name,
+      email,
+      hashedPassword,
+    );
 
-  const user = await userModel.updateUser(req.params.id, name, email);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    }
 
-  res.json(user);
+    const formattedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    res.status(200).json({
+      success: true,
+      user: formattedUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 exports.deleteUser = async (req, res) => {
-  await userModel.deleteUser(req.params.id);
+  try {
+    const user = await userModel.deleteUser(req.params.id);
 
-  res.json({ message: "User deleted" });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "user deleted",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
